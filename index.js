547fb217459eac53cb7ad8f3130895e72b62bda5
ZeK <83297638+ZekXtreme@users.noreply.github.com>
2024-03-14 19:47:14 +0545

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const app = express()
+
+const RED_URL = process.env.RED_URL || "https://www.google.com/"
+
 app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
+    console.log("Just got a request!");
+    res.redirect(RED_URL);
 })
 app.listen(process.env.PORT || 3000)
